f45b48cc9e1d0b96fa454c750d8617cbd540ae1e
rhaenesis <acunainfosec@proton.me>
2025-06-09 11:50:45 -0600

    Added relative path. Not sure if it works

--- email-deactivation-O365/Start-Deactivation.ps1
+++ email-deactivation-O365/Start-Deactivation.ps1
@@ -1,5 +1,5 @@
-&"C:\Email-Deactivation-O365\Steps\Deactivate-PART1.ps1
-&"C:\Email-Deactivation-O365\Steps\Deactivate-PART2.ps1
-&"C:\Email-Deactivation-O365\Steps\Deactivate-PART3.ps1
-&"C:\Email-Deactivation-O365\Steps\Deactivate-PART4.ps1
-&"C:\Email-Deactivation-O365\Steps\Deactivate-PART5.ps1
+&"$PSScriptRoot\Steps\Deactivate-PART1.ps1"
+&"$PSScriptRoot\Steps\Deactivate-PART2.ps1"
+&"$PSScriptRoot\Steps\Deactivate-PART3.ps1"
+&"$PSScriptRoot\Steps\Deactivate-PART4.ps1"
+&"$PSScriptRoot\Steps\Deactivate-PART5.ps1"
